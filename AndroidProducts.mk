99be25139e76d68ca4c187ed06fc351b63e79f31
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-09-24 20:53:59 -0400

    j5ltechn: Update AndroidProducts makefile
    Consider {lineage,rr,omni,pa}_j5ltechn the AOSP way
    
    Change-Id: I01b33da425cc5d80e9f86bd1437910e2f1a4496a

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,6 @@
 LOCAL_PATH := device/samsung/j5ltechn
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_j5ltechn.mk
+	$(LOCAL_DIR)/omni_j5ltechn.mk \
+	$(LOCAL_DIR)/rr_j5ltechn.mk \
+	$(LOCAL_DIR)/pa_j5ltechn.mk \
+	$(LOCAL_DIR)/lineage_j5ltechn.mk
