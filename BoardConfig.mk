7380e949eacb8c5c7b512908a1d57e95097875fc
zqm64 <zqm64@outlook.com>
2020-06-20 00:45:46 +0800

    j3ltectc: Add Inherit from NXP PN547 NFC Support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ TARGET_OTA_ASSERT_DEVICE := j3ltectc
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_j3lte_chnctc_defconfig
 
+# NFC
+-include device/samsung/msm8916-common/nfc/pn547/board.mk
+
 # Radio
 SIM_COUNT := 2
 
--- device.mk
+++ device.mk
@@ -6,31 +6,11 @@ $(call inherit-product, device/samsung/j3-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/j3ltectc
 
+# NFC
+$(call inherit-product, device/samsung/msm8916-common/nfc/pn547/product.mk)
+
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-#NFC
-#PRODUCT_PACKAGES += \
-#	android.hardware.nfc@1.0-impl \
-#	android.hardware.nfc@1.0-service \
-#	com.android.nfc_extras \
-#	libnfc_nci \
-#	libnfc_nci_jni \
-#	nfc_nci.pn54x.msm8916 \
-#	NfcNci \
-#	Tag
-
-# NFC configs
-#PRODUCT_COPY_FILES += \
-#	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
-#	$(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-#	$(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
-
-# Permissions
-#PRODUCT_COPY_FILES += \
-#	frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-#	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-#	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
